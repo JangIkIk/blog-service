156c819e56b2d2dcf56377d38cefc422660bf2b9
Jangs <108041161+JangIkIk@users.noreply.github.com>
2025-06-10 17:04:25 +0900

    [Style 🎨]: Home Ui구조 생성 (#4)
    * style: app/styles 삭제
    
    * style: app/loading 삭제
    
    * style: app/not-found 삭제
    
    * dir: app/(home) 추가
    
    * style: header 태그 변경
    
    * refactor: root layout 구조변경
    
    * style: main 여백추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,103 +1,30 @@
-// import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
-// import './global.scss';
-// import styles from './styles/layout.module.scss';
-// import Providers from './(auth)/providers';
+import Providers from "./(auth)/providers";
+import { auth } from "./(auth)/auth";
 
-// import Header from '@/app/header';
-// import ApplyTheme from '@/shared/ApplyTheme';
-// import { auth } from '@/app/(auth)/auth';
-// import Modal from '@/shared/ui/modal';
-// import { getMetadata } from '@/shared/utils/metadata';
-// import { SearchProvider } from '@/shared/contexts/SearchContext';
-
-// const pretendard = localFont({
-//   src: '../public/fonts/pretendard-medium.woff2',
-// });
-
-// export const generateMetadata = () => {
-//   return getMetadata();
-// };
-
-// // export const metadata: Metadata = {
-// //   title: 'prolog',
-// //   description: '/',
-// //   icons: {
-// //     icon: '/',
-// //   },
-// // };
-
-// export default async function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   const session = await auth();
-
-//   return (
-//     <html lang="ko" className={`${pretendard.className}`}>
-//       <body>
-//         <Providers session={session}>
-//           <ApplyTheme />
-//           <SearchProvider>
-//             <div className={styles.layout}>
-//               <div className={styles.layout__header}>
-//                 <Header />
-//               </div>
-//               <main className={styles.layout__main}>{children}</main>
-//             </div>
-//             <Modal />
-//           </SearchProvider>
-//         </Providers>
-//       </body>
-//     </html>
-//   );
-// }
-import localFont from 'next/font/local';
-import Providers from './(auth)/providers';
-
-// import Header from '@/app/header';
-// import ApplyTheme from '@/shared/ApplyTheme';
-import { auth } from '@/app/(auth)/auth';
-// import Modal from '@/shared/ui/modal';
-// import { getMetadata } from '@/shared/utils/metadata';
-// import { SearchProvider } from '@/shared/contexts/SearchContext';
+import BaseLayout from "@/src/1-app/layout/baseLayout";
+import "@app/global.css";
 
 const pretendard = localFont({
-  src: '../public/fonts/pretendard-medium.woff2',
+  src: "../public/fonts/pretendard-medium.woff2",
 });
 
-// export const generateMetadata = () => {
-//   return getMetadata();
-// };
-
-// export const metadata: Metadata = {
-//   title: 'prolog',
-//   description: '/',
-//   icons: {
-//     icon: '/',
-//   },
-// };
-
-import '@app/global.css';
-import Header from '@/src/1-app/layout/header/ui';
-
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout(props: Props) {
+  const { children } = props;
   const session = await auth();
 
   return (
     <html lang="ko" className={`${pretendard.className}`}>
       <body>
         <Providers session={session}>
-          <main>
-            <Header />
-          </main>
+          <BaseLayout>{children}</BaseLayout>
         </Providers>
       </body>
     </html>
   );
 }
+
+type Props = Readonly<{
+  children: React.ReactNode;
+}>;
