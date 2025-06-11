e5d42defea4dec93cf8b9410abaccb1851434708
junik <wkdwnsdlr37@gmail.com>
2025-06-12 02:34:41 +0900

    refactor: editor 로직분리

--- src/3-widgets/editor/ui/index.tsx
+++ src/3-widgets/editor/ui/index.tsx
@@ -1,75 +1,36 @@
 "use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { RefObject, useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
-
-import {
-  BlockNoteEditor,
-  type PartialBlock,
-  type Block,
-} from "@blocknote/core";
-import { ko } from "@blocknote/core/locales";
-
-import { useCreateBlockNote } from "@blocknote/react";
+import { type PartialBlock } from "@blocknote/core";
 import { BlockNoteView } from "@blocknote/mantine";
-
 import "@blocknote/mantine/style.css";
 import "@blocknote/core/fonts/inter.css";
 
-import { uploadFiles } from "@shared/lib/uploadthing";
-import { UploadButton } from "@shared/lib/uploadthing";
+import { useEditorConfig } from '../hooks/useEditorConfig';
+import { UploadButton } from "../lib/uploadthing";
+
 import { cn } from "@shared/lib/cn";
-
-/*
-  에디터 기본 조건
-  - 테마변경이 가능해야한다.
-  - 노션과 비슷한 기능을 가진다.
-  - 이미지 업로드가 가능해야한다.
-  - 커버를 변경했을때 업로드가 되어야 한다.
-  - 마크다운으로 변환이 가능해야 한다.(서버 저장시 필요)
-    - * 내용은 당연히 마크다운이지만 이 외로 커버이미지, 공개/비공개 등 서버의 테이블에 맞게 보낼수 있는지?
-  - 마크다운을 다시 블록으로 변환이 가능해야 한다.(수정 or 임시저장글 불러올때)
-  - 마크다운을 다시 HTML 블록으로 변환이 가능해야 한다.(SSR로 그릴때)
-
-  * 최소한의 기능을 가지고 구현한다음 커스텀 하기로
-*/
 
 type EditorProps = {
   initContent?: PartialBlock[];
   editable?: boolean;
   url?: string;
+  theme?: "light" | "dark";
+  titleRef: RefObject<HTMLTextAreaElement | null>;
+  coverRef?: RefObject<string>;
+  markdownData?: ( markdownData: string ) => void;
 };
 
 export default function Editor(props: EditorProps) {
-  // 상위에서 useState or useRef를 전달 받아 발행 버튼 클릭시에 유효성 검사 필요
+  const { initContent, coverRef, titleRef, markdownData, editable, url = "https://picsum.photos/1200/800", theme = "light" } = props;
+  const { editor, markdown, changeMarkdown } = useEditorConfig({initContent});
+  const [cover, setCover] = useState<string>(url);
 
-  const {
-    url,
-    initContent,
-    editable,
-  } = props;
-  const [cover, setCover] = useState(url || "https://picsum.photos/1200/800");
-  const [markdown, setMarkdown] = useState<string>("");
-
-  const editor: BlockNoteEditor = useCreateBlockNote({
-    animations: true,
-    defaultStyles: true,
-    trailingBlock: false,
-    dictionary: ko,
-    initialContent: initContent || undefined,
-    uploadFile: async (file: File) => {
-      const [res] = await uploadFiles("imageUploader", { files: [file] });
-      return res.ufsUrl;
-    },
-  });
-
-  const onChange = async () => {
-    const markdown = await editor.blocksToMarkdownLossy(editor.document);
-    setMarkdown(markdown);
-  };
-  useEffect(() => {
-    onChange();
-  }, []);
+  useEffect(()=>{
+    if(cover && coverRef) coverRef.current = cover; 
+    if(markdownData) markdownData(markdown);
+  },[markdown, cover])
 
   return (
     <div className="my-4">
@@ -92,7 +53,6 @@ export default function Editor(props: EditorProps) {
             )}
             onClientUploadComplete={(res)=>{
             setCover(res[0].ufsUrl);
-            console.log(res[0].ufsUrl);
             }}
           />
         </div>
@@ -100,6 +60,7 @@ export default function Editor(props: EditorProps) {
 
       {/* 제목 */}
       <TextareaAutosize
+        ref={titleRef}
         placeholder="제목"
         className="w-full resize-none appearance-none overflow-hidden bg-transparent text-4xl font-bold outline-none py-5 theme-text-1"
       />
@@ -109,8 +70,8 @@ export default function Editor(props: EditorProps) {
         <BlockNoteView
           editor={editor}
           editable={editable}
-          onChange={onChange}
-          theme={"light"}
+          onChange={changeMarkdown}
+          theme={theme}
         />
       </div>
     </div>
