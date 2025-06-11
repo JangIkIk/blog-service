5dd4f5f6ffaaf234c6811c2c46589adc5870aa03
junik <wkdwnsdlr37@gmail.com>
2025-06-12 02:34:18 +0900

    file: useEditorConfig 파일이동

---
+++ src/3-widgets/editor/hooks/useEditorConfig.ts
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteEditor, type PartialBlock } from "@blocknote/core";
+import { ko } from "@blocknote/core/locales";
+import { uploadFiles } from "@/src/3-widgets/editor/lib/uploadthing";
+
+type EditorConfigProps = {
+  initContent?: PartialBlock[];
+};
+
+export const useEditorConfig = (props?: EditorConfigProps) => {
+  const { initContent } = props || {};
+  const [markdown, setMarkdown] = useState<string>("");
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    animations: true,
+    defaultStyles: true,
+    trailingBlock: false,
+    dictionary: ko,
+    initialContent: initContent || undefined,
+    uploadFile: async (file: File) => {
+      const [res] = await uploadFiles("imageUploader", { files: [file] });
+      return res.ufsUrl;
+    },
+  });
+
+  const changeMarkdown = async () => {
+    const markdown = await editor.blocksToMarkdownLossy(editor.document);
+    setMarkdown(markdown);
+  };
+
+  useEffect(()=>{
+    if(initContent) changeMarkdown();
+  },[])
+
+  return { editor, markdown, changeMarkdown };
+};
