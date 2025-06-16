efd9b3fa9fafe8801159fa7467d5cb8d1f56ced5
junik <wkdwnsdlr37@gmail.com>
2025-06-16 20:37:36 +0900

    file: pages/story 이동 및 구조변경

---
+++ src/2-pages/story/ui/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import { type PartialBlock } from "@blocknote/core";
+
+export default function Story() {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/src/3-widgets/editor/index"), { ssr: false }),
+    []
+  );
+
+  return (
+    <div className="px-10">
+      <Editor/>
+    </div>
+  );
+}
+
+
+// const initContent: PartialBlock[] = [
+//     {
+//       type: 'paragraph',
+//       content: '이것은 상위 컴포넌트에서 전달된 초기 데이터입니다.',
+//     },
+//     {
+//       type: 'heading',
+//       props: {
+//         level: 3,
+//       },
+//       content: '데이터 전달 성공!',
+//     },
+//     {
+//         type: 'bulletListItem',
+//         content: '블록노트 배우기',
+//     },
+//     {
+//         type: 'bulletListItem',
+//         content: '초기값 설정하기',
+//     }
+//   ];
