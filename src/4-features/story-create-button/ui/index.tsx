8bd3bf3b55cf691c10928a40eb269abc65ee49ec
junik <wkdwnsdlr37@gmail.com>
2025-06-17 16:06:58 +0900

    feat: StoryCreateButton Button 적용

--- src/4-features/story-create-button/ui/index.tsx
+++ src/4-features/story-create-button/ui/index.tsx
@@ -1,28 +1,36 @@
-import { useCreateStoryApi } from '@entities/use-create-story-api';
-import type { StoryForm } from '@entities/types';
+import { useCreateStoryApi } from "@entities/use-create-story-api";
+import type { StoryForm } from "@entities/types";
+import { Button } from "@shared/ui/Button";
+import { SymbolIcon } from "@radix-ui/react-icons";
 
+export default function StoryCreateButton(props: StoryForm) {
+  const { title, content, aiContent, thumbnailImage, scope = "PUBLIC" } = props;
+  const { status, isLoading, startFetch } = useCreateStoryApi();
 
-export default function StoryCreateButton( props: StoryForm ){
-    const { title, content, aiContent, thumbnailImage, scope = "PUBLIC"} = props;
-    const { status, isLoading, startFetch } = useCreateStoryApi();
+  const submitCreateStory = () => {
+    // model 영역으로 분리 필요
+    if (!title) alert("제목을 입력해주세요");
+    if (!content) alert("내용을 입력해주세요");
 
-    const submitCreateStory = () => {
+    startFetch(props);
+  };
 
-        // model 영역으로 분리 필요
-        if(!title) alert("제목을 입력해주세요");
-        if(!content) alert("내용을 입력해주세요");
+  switch (status) {
+    case 200:
+      break;
+  }
 
-        startFetch(props);
-    }
-
-    switch(status){
-        case 200:
-        break;
-    }
-
-    return(
-        <button className="shrink-0 grow-1 cursor-pointer rounded-lg px-3 py-2 font-bold text-sm bg-brand-soft text-white" onClick={submitCreateStory}>
-          {"발행하기"}
-        </button>
-    );
+  return (
+    <>
+      <Button intent={"soft"} onClick={submitCreateStory} asChild>
+        <div className="flex justify-center">
+          {isLoading ? (
+            <SymbolIcon className="animate-spin" />
+          ) : (
+            <span>발행하기</span>
+          )}
+        </div>
+      </Button>
+    </>
+  );
 }
