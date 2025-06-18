5153f56d323eb6f6e2b85803268ad6d1c9033180
junik <wkdwnsdlr37@gmail.com>
2025-06-18 16:20:46 +0900

    refactor: core.ts 상수 및 주석

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,43 +1,44 @@
-// https://docs.uploadthing.com/getting-started/appdir (next.js App Router)
-// https://docs.uploadthing.com/file-routes (File Routes)
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types#image (Media types)
+// package
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import { auth } from '@app/auth';
+// layer
+import { FILE_MIME_TYPES, FILE_SIZE, FILE_COUNT} from '@shared/consts/const'
+// type
+type OurFileRouter = typeof ourFileRouter;
+
 
 const fileConfig = createUploadthing();
-
+/**
+ * @see {@link https://docs.uploadthing.com/getting-started/appdir (next.js App Router)}
+ * @see {@link https://docs.uploadthing.com/file-routes (File Routes)}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types#image (Media types)}
+ * @Desc
+ * uploadthing 파일라우터
+*/
 const ourFileRouter = {
   editorUploadr: fileConfig({
-    // image가 가지는 기본적인 MIME중 image/gi, image/svg+xmlf 제한
-    "image/jpeg": { maxFileSize: "4MB", maxFileCount: 1},
-    "image/png": { maxFileSize: "4MB", maxFileCount: 1},
-    "image/webp": { maxFileSize: "4MB", maxFileCount: 1},
+    "image/jpeg": { maxFileSize: "4MB", maxFileCount: FILE_COUNT},
+    "image/png": { maxFileSize: "4MB", maxFileCount: FILE_COUNT},
+    "image/webp": { maxFileSize: "4MB", maxFileCount: FILE_COUNT},
   })
     // 추후 런타입환경을 고려할필요가 있음 (사용자에 따른 입력을 받아야하는 경우?)
     // .input(z.object({ postId: z.string() }))
-    // 미들웨어를 사용하여 업로드 전 유저 인증 로직 & 클라이언트에서 받을 데이터 확인
     .middleware(async (data) => {
       const { files, input } = data;
-      // 테스트로 인한 임시보류
-      // const session = await auth();
-      // if (!session)
-      // throw new UploadThingError(
-      //   "You must be logged in to upload a profile picture",
-      // );
-      // return { userId: session}
+      
+      const file = files[0];
+      if(files.length > FILE_COUNT) throw new UploadThingError("COUNT");
+      if(!FILE_MIME_TYPES.includes(file.type)) throw new UploadThingError("TYPE");
+      if(file.size > FILE_SIZE) throw new UploadThingError("SIZE");
+
       return {}
     })
-    // 업로드 실패시
     .onUploadError((data) => {
       const { error, fileKey } = data;
     })
-    // 업로드 완료시
     .onUploadComplete((data) => {
       const { metadata, file } = data;
     }),
 } satisfies FileRouter;
 
-
-type OurFileRouter = typeof ourFileRouter;
 export { ourFileRouter, type OurFileRouter };
