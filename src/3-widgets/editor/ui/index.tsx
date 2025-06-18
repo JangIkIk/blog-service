d1f79cc9ddf011e8d498bc1e6f7dfa24cc95e0e1
junik <wkdwnsdlr37@gmail.com>
2025-06-18 17:45:50 +0900

    refactor: editor 업로드 버튼 변경

--- src/3-widgets/editor/ui/index.tsx
+++ src/3-widgets/editor/ui/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+// package
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
@@ -6,65 +7,54 @@ import { type PartialBlock } from "@blocknote/core";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import "@blocknote/core/fonts/inter.css";
-
-import { useEditorConfig } from '../hooks/useEditorConfig';
-import { UploadButton } from "../lib/uploadthing";
-
-import { cn } from "@shared/lib/cn";
-import StoryCreateButton from '@features/story-create-button/ui';
-import { StoryForm } from '@entities/types';
-
-
+// slice
+import { useEditorConfig } from "../lib/useEditorConfig";
+// layer
+import UploadButton, { useUploadedUrl } from '@features/image-upload-button'
+import StoryCreateButton from "@features/story-create-button";
+import { StoryForm } from "@entities/types";
+// type
 type EditorProps = {
   initContent?: PartialBlock[];
   editable?: boolean;
   url?: string;
   theme?: "light" | "dark";
-  markdownData?: ( markdownData: string ) => void;
+  markdownData?: (markdownData: string) => void;
 };
-
+/**
+ * @Desc
+ * blocknote 에디터
+ */ 
 function Editor(props: EditorProps) {
-  const { initContent, markdownData, editable, url = "https://picsum.photos/1200/800", theme = "light" } = props;
-  const { editor, markdown, changeMarkdown } = useEditorConfig({initContent});
-  const [cover, setCover] = useState<string>(url);
+  const { initContent, markdownData, editable, url, theme = "light"} = props;
+  const { editor, markdown, changeMarkdown } = useEditorConfig({ initContent });
+  const uploadedCover = useUploadedUrl( state => state.imageUrl);
+
   const [storyData, setStoryData] = useState<StoryForm>({
-      title: "initTitle",
-      content: "initContent",
-      aiContent: null,
-      thumbnailImage: null,
-      scope: "PUBLIC"
-    })
+    title: "initTitle",
+    content: "initContent",
+    aiContent: null,
+    thumbnailImage: null,
+    scope: "PUBLIC",
+  });
 
-
-  useEffect(()=>{
-    // if(cover && coverRef) coverRef.current = cover; 
-    if(markdownData) markdownData(markdown);
-  },[markdown, cover])
+  useEffect(() => {
+    if (markdownData) markdownData(markdown);
+  }, [markdown]);
 
   return (
     <div className="my-4">
-      <div className="relative w-full h-[300px] theme-bg-2 rounded-lg group">
-      {/* 커버이미지 */}
-      <Image
-            src={cover}
+      <div className="relative w-full h-50 theme-bg-2 rounded-lg group">
+        { uploadedCover && <Image
+            src={uploadedCover}
             alt="cover"
             fill
             className="object-cover rounded-lg"
             sizes="100vw"
-          />
-        {/* 업로드 버튼 */}
-        <div>
-          <UploadButton
-            endpoint="imageUploader"
-            content={{ button: "커버변경" }}
-            className={cn(
-              "w-fit ut-allowed-content:hidden absolute right-2 bottom-2 opacity-0 transition-opacity ut-button:bg-brand-main ut-button:rounded-lg group-hover:opacity-100"
-            )}
-            onClientUploadComplete={(res)=>{
-            setCover(res[0].ufsUrl);
-            }}
-          />
-        </div>
+            />}
+          <div className="absolute right-3 bottom-3">
+            <UploadButton/>
+          </div>
       </div>
 
       {/* 제목 */}
@@ -82,15 +72,10 @@ function Editor(props: EditorProps) {
           theme={theme}
         />
       </div>
-      
 
       <div className="flex flex-col gap-3 justify-end fixed right-0 bottom-0 p-4">
-        <StoryCreateButton {...storyData}/>
-        {["발행하기", "임시저장", "취소하기", "공개/비공개"].map((text) => (
-            <button key={text} className="shrink-0 grow-1 cursor-pointer rounded-lg px-3 py-2 font-bold text-sm bg-brand-soft text-white">
-          {text}
-        </button>    
-        ))}
+        <StoryCreateButton {...storyData} />
+        {/* 임시저장, 취소하기, 공개/비공개 */}
       </div>
     </div>
   );
