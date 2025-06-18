4b946b6fc09104fd0d7c58ab24fba500308ce403
junik <wkdwnsdlr37@gmail.com>
2025-06-18 16:33:03 +0900

    setting: blocknote 설정변경

--- src/3-widgets/editor/lib/useEditorConfig.ts
+++ src/3-widgets/editor/lib/useEditorConfig.ts
@@ -1,17 +1,24 @@
 "use client";
+// package
 import { useEffect, useState } from "react";
+import { generateReactHelpers } from "@uploadthing/react";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteEditor, type PartialBlock } from "@blocknote/core";
 import { ko } from "@blocknote/core/locales";
-import { uploadFiles } from "@/src/3-widgets/editor/lib/uploadthing";
-
+// layer
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+// type
 type EditorConfigProps = {
   initContent?: PartialBlock[];
 };
-
+/**
+ * @Desc
+ * blocknote 설정
+*/
 const useEditorConfig = (props?: EditorConfigProps) => {
   const { initContent } = props || {};
   const [markdown, setMarkdown] = useState<string>("");
+  const { uploadFiles } = generateReactHelpers<OurFileRouter>();
 
   const editor: BlockNoteEditor = useCreateBlockNote({
     animations: true,
@@ -20,7 +27,7 @@ const useEditorConfig = (props?: EditorConfigProps) => {
     dictionary: ko,
     initialContent: initContent || undefined,
     uploadFile: async (file: File) => {
-      const [res] = await uploadFiles("imageUploader", { files: [file] });
+      const [res] = await uploadFiles("editorUploadr", { files: [file] });
       return res.ufsUrl;
     },
   });
