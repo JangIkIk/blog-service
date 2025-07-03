ad46aa5a9215ed3f34f0244e4d0110431a842cde
junik <wkdwnsdlr37@gmail.com>
2025-07-03 10:12:54 +0900

    feat: postSchema 추가

--- src/5-entities/story/schemas/index.ts
+++ src/5-entities/story/schemas/index.ts
@@ -1,16 +1,24 @@
 import { type PartialBlock, type Block } from "@blocknote/core";
 import { z } from 'zod';
 
-const storyScopeEnum = z.enum(["PUBLIC", "PRIVATE", "FRIENDS"]);
-const storySchema = z.object({
+const z_PostScope = z.enum(["PUBLIC", "PRIVATE", "FRIENDS"]);
+const z_InputPost = z.object({
     title: z.string().trim().min(1, "제목은 필수입니다"),
     content: z.array(z.custom<PartialBlock>()).min(1, "내용은 필수입니다"),
     aiContent: z.string().nullable(),
     thumbnailImage: z.string().nullable(),
-    scope: storyScopeEnum,
+    scope: z_PostScope,
+});
+const z_RequestPost = z.object({
+    title: z.string().trim().min(1, "제목은 필수입니다"),
+    content: z.string().min(1, "내용은 필수입니다"),
+    aiContent: z.string().nullable(),
+    thumbnailImage: z.string().nullable(),
+    scope: z_PostScope,
 });
 
 export {
-    storyScopeEnum,
-    storySchema,
+    z_PostScope,
+    z_InputPost,
+    z_RequestPost,
 };
